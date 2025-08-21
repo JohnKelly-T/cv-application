aace09ea8daeaec7fd98882250ed8c92d9fa125c
john-kelly <johnkellytemena@gmail.com>
2025-08-21 19:15:29 +0800

    Add contact dummy data

--- src/default-data.js
+++ src/default-data.js
@@ -2,5 +2,11 @@ export const defaultData = {
   firstName: 'John Kelly',
   lastName: 'Temeña',
   middleInitial: 'C.',
-  jobTitle: 'Front-end Developer'
+  jobTitle: 'Front-end Developer',
+  summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  number: '0912-345-678',
+  email: 'example@gmail.com',
+  website: 'amazingportfolio.com',
+  github: 'github.com/userName',
+  location: 'Anywhere 123 Any St.'
 };
