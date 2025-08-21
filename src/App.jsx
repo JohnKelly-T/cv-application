3a08d7e74034604b9808094ccc8ee4d547bf127b
john-kelly <johnkellytemena@gmail.com>
2025-08-21 14:46:08 +0800

    Add base containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
 
   return (
     <>
+    <div className="form-container"></div>
+    <div className="cv-container"></div>
     </>
   )
 }
