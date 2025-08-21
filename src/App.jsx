287927fb31693f2f48a103e60277d9caf66f5cba
john-kelly <johnkellytemena@gmail.com>
2025-08-21 17:45:00 +0800

    Add ResumePreview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import ResumePreview from './components/ResumePreview';
+import { defaultData } from './default-data';
 
 function App() {
+  const [data, setData] = useState(defaultData);
 
   return (
     <>
     <div className="form-container"></div>
-    <div className="cv-container"></div>
+    <div className="cv-container">
+      <ResumePreview data={data}></ResumePreview>
+    </div>
     </>
   )
 }
