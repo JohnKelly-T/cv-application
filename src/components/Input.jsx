4deb85af3009fd73cb6d0b716abbe324d5061eea
john-kelly <johnkellytemena@gmail.com>
2025-08-26 17:25:37 +0800

    Refactor component to accept data and dataProperty props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,14 @@
-function Input({label, type, value, onChange}) {  
+function Input({ data, dataProperty, label, type, onChange }) {
   return (
     <label className="input-label">
       {label}
-      <input type={type} value={value ? value : ''} onChange={onChange}/>
-
+      <input
+        type={type}
+        value={data[dataProperty]}
+        onChange={(e) => onChange({ ...data, [dataProperty]: e.target.value })}
+      />
     </label>
-    
-  )
+  );
 }
 
 export default Input;
