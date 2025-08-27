0108467f28f3aa1d53d713209963f3e4213127bc
john-kelly <johnkellytemena@gmail.com>
2025-08-27 17:43:09 +0800

    Format with prettier

--- src/default-data.js
+++ src/default-data.js
@@ -1,62 +1,78 @@
 export const defaultData = {
-  firstName: 'John Kelly',
-  lastName: 'Temeña',
-  middleInitial: 'C.',
-  jobTitle: 'Front-end Developer',
-  summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  phone: '0912-345-678',
-  email: 'example@gmail.com',
-  website: 'amazingportfolio.com',
-  github: 'github.com/userName',
-  location: 'Anywhere 123 Any St.',
+  firstName: "John Kelly",
+  lastName: "Temeña",
+  middleInitial: "C.",
+  jobTitle: "Front-end Developer",
+  summary:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  phone: "0912-345-678",
+  email: "example@gmail.com",
+  website: "amazingportfolio.com",
+  github: "github.com/userName",
+  location: "Anywhere 123 Any St.",
   experiences: [
     {
-      company: 'Company1',
-      position: 'Front-end engineer',
-      startYear: '2024',
-      endYear: '2025',
-      description: 'Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris'
+      company: "Company1",
+      position: "Front-end engineer",
+      startYear: "2024",
+      endYear: "2025",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris",
     },
     {
-      company: 'Company2',
-      position: 'Front-end engineer',
-      startYear: '2023',
-      endYear: '2024',
-      description: 'Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris'
+      company: "Company2",
+      position: "Front-end engineer",
+      startYear: "2023",
+      endYear: "2024",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris",
     },
     {
-      company: 'Company3',
-      position: 'Front-end engineer',
-      startYear: '2023',
-      endYear: '2024',
-      description: 'Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-    }
-  ], 
+      company: "Company3",
+      position: "Front-end engineer",
+      startYear: "2023",
+      endYear: "2024",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    },
+  ],
   educations: [
     {
-      school: 'School 1',
-      course: 'BS Computer Science',
-      startYear: '2024',
-      endYear: '2025',
-      description: 'Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris'
+      school: "School 1",
+      course: "BS Computer Science",
+      startYear: "2024",
+      endYear: "2025",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris",
     },
     {
-      school: 'School 2',
-      course: 'STEM',
-      startYear: '2023',
-      endYear: '2024',
-      description: 'Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris'
-    }
-  ], 
-  skills: ['Skill 1 here', 'Another skill here', 'Third skill', 'More skill'],
+      school: "School 2",
+      course: "STEM",
+      startYear: "2023",
+      endYear: "2024",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adici elit, sed do\nEiusmod tempor incididunt ut labore et dolore magna aliqua\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+    },
+  ],
+  skills: ["Skill 1 here", "Another skill here", "Third skill", "More skill"],
   more: [
     {
-      heading: 'TOOLS', 
-      list: ['First tool here', 'Another tool there', 'Cool Tool', 'And more tools']
+      heading: "TOOLS",
+      list: [
+        "First tool here",
+        "Another tool there",
+        "Cool Tool",
+        "And more tools",
+      ],
     },
     {
-      heading: 'INTERESTS', 
-      list: ['Hobby here', 'Interesting topic here', 'Interested field', 'More interests']
-    }
-  ]
+      heading: "INTERESTS",
+      list: [
+        "Hobby here",
+        "Interesting topic here",
+        "Interested field",
+        "More interests",
+      ],
+    },
+  ],
 };
