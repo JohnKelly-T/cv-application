891b679a470e201f33285b2c015a89d95a04f33b
john-kelly <johnkellytemena@gmail.com>
2025-08-28 15:11:34 +0800

    Fix error when data.experiences is undefined

--- src/components/WorkExperienceForm.jsx
+++ src/components/WorkExperienceForm.jsx
@@ -26,51 +26,53 @@ function WorkExperienceForm({ data, setData, isActive, onShow }) {
       onShow={onShow}
     >
       <div className="form" id="work-experience">
-        {data.experiences.map((experience, index) => {
-          return (
-            <ItemAccordion
-              key={index}
-              label={
-                experience.position +
-                (experience.company ? ", " + experience.company : "")
-              }
-              isActive={activeItemIndex === index}
-              onShow={(i = index) => setActiveItemIndex(i)}
-            >
-              <div className="work-experience-item">
-                <Input
-                  label="Position"
-                  value={experience.position}
-                  onChange={(e) => handleOnchange(index, "position", e)}
-                />
+        {data.experiences
+          ? data.experiences.map((experience, index) => {
+              return (
+                <ItemAccordion
+                  key={index}
+                  label={
+                    experience.position +
+                    (experience.company ? ", " + experience.company : "")
+                  }
+                  isActive={activeItemIndex === index}
+                  onShow={(i = index) => setActiveItemIndex(i)}
+                >
+                  <div className="work-experience-item">
+                    <Input
+                      label="Position"
+                      value={experience.position}
+                      onChange={(e) => handleOnchange(index, "position", e)}
+                    />
 
-                <Input
-                  label="Company"
-                  value={experience.company}
-                  onChange={(e) => handleOnchange(index, "company", e)}
-                />
+                    <Input
+                      label="Company"
+                      value={experience.company}
+                      onChange={(e) => handleOnchange(index, "company", e)}
+                    />
 
-                <Input
-                  label="Start Date"
-                  value={experience.startDate}
-                  onChange={(e) => handleOnchange(index, "startDate", e)}
-                />
+                    <Input
+                      label="Start Date"
+                      value={experience.startDate}
+                      onChange={(e) => handleOnchange(index, "startDate", e)}
+                    />
 
-                <Input
-                  label="End Date"
-                  value={experience.endDate}
-                  onChange={(e) => handleOnchange(index, "endDate", e)}
-                />
+                    <Input
+                      label="End Date"
+                      value={experience.endDate}
+                      onChange={(e) => handleOnchange(index, "endDate", e)}
+                    />
 
-                <TextArea
-                  label="Description"
-                  value={experience.description}
-                  onChange={(e) => handleOnchange(index, "description", e)}
-                />
-              </div>
-            </ItemAccordion>
-          );
-        })}
+                    <TextArea
+                      label="Description"
+                      value={experience.description}
+                      onChange={(e) => handleOnchange(index, "description", e)}
+                    />
+                  </div>
+                </ItemAccordion>
+              );
+            })
+          : null}
 
         <button className="add-item-button">
           <AddIcon /> Add work experience
