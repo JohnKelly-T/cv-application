84fa321ce58386c1c3fbc31d69e7d124fe26c238
john-kelly <johnkellytemena@gmail.com>
2025-08-28 16:57:33 +0800

    Fix error when description is undefined

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,25 +1,25 @@
-function Education ({school, startYear, endYear, description}) {
-  let period = '';
+function Education({ school, startYear, endYear, description }) {
+  let period = "";
 
   if (startYear && endYear) {
-    period = startYear + ' - ' + endYear;
+    period = startYear + " - " + endYear;
   } else if (startYear) {
     period += startYear;
   } else if (endYear) {
     period += endYear;
   }
-  
+
   return (
     <>
       <div className="experience">
         <div className="school">{school}</div>
-        <div className="period">
-          {period}
-        </div>
+        <div className="period">{period}</div>
         <ul className="description">
-          {description.split('\n').map( (line, index) => {
-            return <li key={index}>{line}</li>
-          })}
+          {description
+            ? description.split("\n").map((line, index) => {
+                return <li key={index}>{line}</li>;
+              })
+            : null}
         </ul>
       </div>
     </>
