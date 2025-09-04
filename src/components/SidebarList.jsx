7d999b87519d8574eaab40fcc09ea7ad4af56fc7
john-kelly <johnkellytemena@gmail.com>
2025-09-04 16:30:47 +0800

    Add links to section items

--- src/components/SidebarList.jsx
+++ src/components/SidebarList.jsx
@@ -1,19 +1,21 @@
-
 function SideBarList({ list }) {
   return (
     <>
-      {
-      list && list.length !== 0 ?
-      <ul className="sidebar-list">
-        {
-          list.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))
-        }
-      </ul> : null
-      }
+      {list && list.length !== 0 ? (
+        <ul className="sidebar-list">
+          {list.map((item, index) =>
+            typeof item === "object" ? (
+              <li key={index}>
+                <a href={item.url}>{item.linkText}</a>
+              </li>
+            ) : (
+              <li key={index}>{item}</li>
+            ),
+          )}
+        </ul>
+      ) : null}
     </>
-  )
+  );
 }
 
 export default SideBarList;
